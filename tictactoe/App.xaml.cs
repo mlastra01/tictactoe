6baed2d012d9475f4d3d53f8bceea0d16bb36d24
mlastra01 <mlastra@est.frba.utn.edu.ar>
2021-03-26 23:24:17 -0300

    Primera subida

--- tictactoe/App.xaml.cs
+++ tictactoe/App.xaml.cs
@@ -13,5 +13,6 @@ namespace tictactoe
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ tictactoe/MarkType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The type of value a cell in the game is currently at
+/// </summary>
+
+namespace tictactoe
+{
+    public enum MarkType
+    {
+        /// The cell hasnt been clicked yet
+        Free,
+        ///The cell is  a O
+        Nought,
+        ///The cell is a X
+        Cross
+        
+    }
+}
